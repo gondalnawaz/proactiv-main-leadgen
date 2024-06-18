4ece31125e864bc072a8e6c774dc7cc6a9840608
HasanBupa <108452981+HasanBupa@users.noreply.github.com>
2024-06-18 15:27:03 +0500

    Issues Fixes

--- app/components/CardSamples.jsx
+++ app/components/CardSamples.jsx
@@ -24,17 +24,15 @@ const CardSamples = () => {
 
                 <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673385/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_hra8f5_nj0tcd.png" alt="" /> 
                 <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688675362/CS7_lgs3ja_koutr4.jpg" alt="" />
- */}
-                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbdkbwkhr/image/upload/v1718057922/ewfdakbxuxlw66gteoin.png" alt="" width={215} height={120} />
-                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbdkbwkhr/image/upload/v1718057931/x6lftx6lchpclv6ksp1o.png" alt="" width={215} height={120} />
-                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbdkbwkhr/image/upload/v1718057932/igumamxkowessuhnmjh8.png" alt="" width={215} height={120} />
+                */}
+                    
                     <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673385/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_2_fhirv3_gqmrdh.png" alt="" width={215} height={120} />
-                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673384/CS4_oaeyvy_q8rk8u.jpg" alt="" width={215} height={120} />
+                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbdkbwkhr/image/upload/v1718704045/WhatsApp_Image_2024-06-11_at_5.56.22_PM_zslhli.jpg" alt="" width={215} height={120} />
                     <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673385/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_3_amaojv_yros80.png" alt="" width={215} height={120} />
-                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673384/CS5_vgxtcv_jxgqmp.png" alt="" width={220} height={120} />
+                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbdkbwkhr/image/upload/v1718704045/WhatsApp_Image_2024-06-11_at_5.56.22_PM_1_xjpyzm.jpg" alt="" width={220} height={120} />
                     <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673384/CS3_gwphcx_lztewc.jpg" alt="" width={220} height={120} />
                     <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673385/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_xkkal3_gtknol.png" alt="" width={220} height={120}/>
-                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673384/CS6_zurcdm_dpnsqe.jpg" alt="" width={220} height={120} />
+                    <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbdkbwkhr/image/upload/v1718704044/WhatsApp_Image_2024-06-11_at_5.56.22_PM_2_vqhhlv.jpg" alt="" width={220} height={120} />
                     <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673385/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_4_s09qpv_kog2c4.png" alt="" width={220} height={120} />
                     <Image className="overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbew7ibhf/image/upload/v1688673385/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_hra8f5_nj0tcd.png" alt=""width={220} height={120} />
                                
